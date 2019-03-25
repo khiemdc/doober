ae784e124592c08d67871d1052818ebb7d714d48
khiem.v.le <lek@deea.thebuffalogroup.com>
2019-03-25 17:08:44 -0400

    updated

--- src/app/service/helper.service.ts
+++ src/app/service/helper.service.ts
@@ -12,7 +12,8 @@ constructor(private httpClient: HttpClient) { }
 
   getCountries(): Observable<ICountry[]> {
   // return this.http.get<ICountry[]>('http://localhost:41516/api');
-  return this.httpClient.get<ICountry[]>('../../src/app/shared/data/countries.json');
+  // return this.httpClient.get<ICountry[]>('../../src/app/shared/data/countries.json');
+  return this.httpClient.get<ICountry[]>('assets/countries.json');
 }
 
 }
